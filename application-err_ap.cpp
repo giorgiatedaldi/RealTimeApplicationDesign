16528e543c359b50ea39a81ed102b6587a001e17
Giorgia Tedaldi <75640810+giorgiatedaldi@users.noreply.github.com>
2023-02-28 22:30:03 +0100

    Update application-err_ap.cpp

--- application-err_ap.cpp
+++ application-err_ap.cpp
@@ -1,7 +1,7 @@
 /**
  * @file application-er_ap.cpp
- * @author AMEDEO BERTUZZI 340922
- * @author GIORGIA TEDALDI 339642
+ * @author AMEDEO BERTUZZI 
+ * @author GIORGIA TEDALDI 
  */
 
 #include "executive.h"
@@ -78,8 +78,7 @@ void task4()
 	std::cout << debug.str();
 }
 
-/* Nota: nel codice di uno o piu' task periodici e' lecito chiamare Executive::ap_task_request() */
-
+/* Note: in the code of one or more periodic task it is allowed to call Executive::ap_task_request()*/
 void ap_task()
 {
 	auto start = std::chrono::steady_clock::now();
